0b83bb4ed2fde978cd2dfb8b94ed678fc17e9469
Andy Monroe <jayhawker805@gmail.com>
2016-09-27 11:25:48 -0500

    Add overloaded methods in MathTools

--- MathTools.cpp
+++ MathTools.cpp
@@ -1,3 +1,22 @@
 
 
 #include "MathTools.h"
+#include "MathToolsInterface.h"
+
+int intersectLineUnitCircle(double d, double e, double f, double xy[2][])
+    throw (NoIntersection) = 0
+{
+
+}
+
+void roomDimensions(double Area, double Extra, double minRequiredLength,
+    double& width, double& length) throw (CannotDetermineRoomDimensions) = 0
+{
+
+}
+
+int solveQuadratic(double a, double b, double c, double roots[])
+    throw (NegativeDiscriminant) = 0
+{
+
+}
--- MathTools.h
+++ MathTools.h
@@ -6,7 +6,17 @@
 class MathTools: public MathToolsInterface
 {
     public:
+        int intersectLineUnitCircle(double d, double e, double f, double xy[2][])
+    		throw (NoIntersection) = 0;
+
+        void roomDimensions(double Area, double Extra, double minRequiredLength,
+        	double& width, double& length) throw (CannotDetermineRoomDimensions) = 0;
+
+        int solveQuadratic(double a, double b, double c, double roots[])
+			throw (NegativeDiscriminant) = 0;
 
     private:
 
-}
+};
+
+#endif
