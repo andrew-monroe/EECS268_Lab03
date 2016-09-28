1254ede8fa3292221e122396e8119008eb18d11f
Andy Monroe <jayhawker805@gmail.com>
2016-09-28 07:04:15 -0500

    Define solveQuadratic method

--- MathTools.cpp
+++ MathTools.cpp
@@ -3,20 +3,47 @@
 #include "MathTools.h"
 #include "MathToolsInterface.h"
 
-int intersectLineUnitCircle(double d, double e, double f, double xy[2][])
-    throw (NoIntersection) = 0
+int MathTools::intersectLineUnitCircle(double d, double e, double f, double xy[2][])
+    throw (NoIntersection)
 {
 
 }
 
-void roomDimensions(double Area, double Extra, double minRequiredLength,
-    double& width, double& length) throw (CannotDetermineRoomDimensions) = 0
+void MathTools::roomDimensions(double Area, double Extra, double minRequiredLength,
+    double& width, double& length) throw (CannotDetermineRoomDimensions)
 {
 
 }
 
-int solveQuadratic(double a, double b, double c, double roots[])
-    throw (NegativeDiscriminant) = 0
+int MathTools::solveQuadratic(double a, double b, double c, double roots[])
+    throw (NegativeDiscriminant)
 {
+    try
+    {
+        if (getDiscriminant() < 0)
+            throw (NegativeDiscriminant e);
 
+        if (getDiscriminant() == 0)
+        {
+            roots[0] = -b / (2*a);
+
+            return(1);
+        }
+        else
+        {
+            roots[0] = ((-b + math.sqrt(getDiscriminant())) / (2*a));
+            roots[1] = ((-b - math.sqrt(getDiscriminant())) / (2*a));
+
+            return(2);
+        }
+    }
+    catch (NegativeDiscriminant& e)
+    {
+
+    }
+}
+
+double MathTools::getDiscriminant(double a, double b, double c)
+{
+    return(b*b - 4*a*c);
 }
