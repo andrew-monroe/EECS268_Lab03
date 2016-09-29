072fb0c3b1f022f33d053e125814dbb644e07485
Andy Monroe <jayhawker805@gmail.com>
2016-09-29 09:48:33 -0500

    Complete MathTools header file

--- MathTools.h
+++ MathTools.h
@@ -3,20 +3,24 @@
 #ifndef MathTools_h
 #define MathTools_h
 
+#include <stdexcept>
+#include <math.h>
+#include "MathToolsInterface.h"
+
 class MathTools: public MathToolsInterface
 {
     public:
-        int intersectLineUnitCircle(double d, double e, double f, double xy[2][])
-    		throw (NoIntersection) = 0;
+        int intersectLineUnitCircle(double d, double e, double f, double** xy)
+    		throw (NoIntersection);
 
         void roomDimensions(double Area, double Extra, double minRequiredLength,
-        	double& width, double& length) throw (CannotDetermineRoomDimensions) = 0;
+        	double& width, double& length) throw (CannotDetermineRoomDimensions);
 
         int solveQuadratic(double a, double b, double c, double roots[])
-			throw (NegativeDiscriminant) = 0;
+			throw (NegativeDiscriminant);
 
     private:
-
+        double getDiscriminant(double a, double b, double c);
 };
 
 #endif
