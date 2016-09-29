70b66e931c82cd3b73a597e21049ecbdcb79f5f9
Andy Monroe <jayhawker805@gmail.com>
2016-09-29 09:50:41 -0500

    Create implementation for intersectLineUnitCircle

--- MathTools.cpp
+++ MathTools.cpp
@@ -1,12 +1,38 @@
 
 
 #include "MathTools.h"
-#include "MathToolsInterface.h"
+#include "NegativeDiscriminant.h"
+#include "NoIntersection.h"
+#include "CannotDetermineRoomDimensions.h"
 
-int MathTools::intersectLineUnitCircle(double d, double e, double f, double xy[2][])
+int MathTools::intersectLineUnitCircle(double d, double e, double f, double** xy)
     throw (NoIntersection)
 {
+    try
+    {
+        switch (solveQuadratic(d*d + e*e, e*f, f*f - d*d, xy[0]))
+        {
+            case 0:
+                throw (NoIntersection("No intersection here."));
+                break;
+            case 1:
+                xy[1][0] = (-f - e*xy[0][0]) / d;
 
+                return(1);
+                break;
+            case 2:
+                xy[1][0] = (-f - e*xy[0][0]) / d;
+
+                xy[1][1] = (-f - e*xy[0][1]) / d;
+
+                return(2);
+                break;
+        }
+    }
+    catch(NoIntersection& e)
+    {
+        return(0);
+    }
 }
 
 void MathTools::roomDimensions(double Area, double Extra, double minRequiredLength,
@@ -20,10 +46,10 @@ int MathTools::solveQuadratic(double a, double b, double c, double roots[])
 {
     try
     {
-        if (getDiscriminant() < 0)
-            throw (NegativeDiscriminant e);
+        if (getDiscriminant(a, b, c) < 0)
+            throw (NegativeDiscriminant("Negative discriminant foud."));
 
-        if (getDiscriminant() == 0)
+        if (getDiscriminant(a, b, c) == 0)
         {
             roots[0] = -b / (2*a);
 
@@ -31,15 +57,15 @@ int MathTools::solveQuadratic(double a, double b, double c, double roots[])
         }
         else
         {
-            roots[0] = ((-b + math.sqrt(getDiscriminant())) / (2*a));
-            roots[1] = ((-b - math.sqrt(getDiscriminant())) / (2*a));
+            roots[0] = ((-b + sqrt(getDiscriminant(a, b, c))) / (2*a));
+            roots[1] = ((-b - sqrt(getDiscriminant(a, b, c))) / (2*a));
 
             return(2);
         }
     }
     catch (NegativeDiscriminant& e)
     {
-
+        return(0);
     }
 }
 
